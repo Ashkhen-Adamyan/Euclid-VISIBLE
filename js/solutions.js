3f7dbfb6de777cd4c89a59e42d25f1259933916c
Ashkhen-Adamyan <92922722+Ashkhen-Adamyan@users.noreply.github.com>
2022-02-06 17:30:19 +0300

    Add files via upload

---
+++ js/solutions.js
@@ -0,0 +1,24 @@
+// section 'solutions'
+
+const swiper = new Swiper('.swiper', {
+  autoplay: {
+    delay: 4000,
+  },
+    loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    hide: true,
+  },
+});
